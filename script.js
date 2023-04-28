bba0b739cb4e04b88f89c45f7d1c023d62b76133
sivlin <sivlin@gmail.com>
2023-04-27 22:31:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,35 @@ function incrementGold() {
 	gold += 1;
 	document.getElementById("gold").innerText = gold;
 }
+
+const nightModeToggle = document.getElementById("night-mode-toggle");
+const body = document.body;
+
+nightModeToggle.addEventListener("change", function() {
+	if (this.checked) {
+		body.classList.add("night-mode");
+	} else {
+		body.classList.remove("night-mode");
+	}
+});
+
+const settingsButton = document.querySelector('nav a[href="#"]');
+const settingsMenu = document.createElement("div");
+settingsMenu.className = "settings";
+settingsMenu.innerHTML = `
+	<label>
+		<input type="checkbox" id="night-mode-toggle">
+		Night mode
+	</label>
+`;
+
+settingsButton.addEventListener("click", function(e) {
+	e.preventDefault();
+	document.body.appendChild(settingsMenu);
+});
+
+document.addEventListener("click", function(e) {
+	if (e.target.closest(".settings") !== settingsMenu && e.target !== settingsButton) {
+		settingsMenu.remove();
+	}
+});
