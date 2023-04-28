447751faf0fb8fc050ab218606f20d4066c08dfe
sivlin <sivlin@gmail.com>
2023-04-27 21:34:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,42 +8,35 @@ const upgradeButton = document.getElementById('upgrade-button');
 const upgradeCostSpan = document.getElementById('upgrade-cost');
 const goldPerSecondSpan = document.getElementById('gold-per-second');
 
-// Update gold counter
+// Update gold counter and stats
 function updateGoldCounter() {
-	gold += goldPerSecond;
-	goldCounter.textContent = gold;
-	
-	if (gold >= upgradeCost) {
-		upgradeButton.disabled = false;
-		upgradeButton.textContent = `Upgrade (Cost: ${upgradeCost} gold)`;
-		upgradeButton.classList.add('clickable');
-	} else {
-		upgradeButton.disabled = true;
-		upgradeButton.textContent = `Upgrade (Cost: ${upgradeCost} gold)`;
-		upgradeButton.classList.remove('clickable');
-	}
-	
-	// Update gold per second and upgrade cost based on purchase
-	if (gold >= upgradeCost) {
-		goldPerSecond *= 1.5;
-		goldPerSecond = Math.round(goldPerSecond * 100) / 100;
-		goldPerSecondSpan.textContent = goldPerSecond;
-		upgradeCost *= 1.2;
-		upgradeCost = Math.round(upgradeCost);
-		upgradeCostSpan.textContent = upgradeCost;
-	}
+  gold += goldPerSecond;
+  goldCounter.textContent = gold;
+
+  // Update gold per second
+  goldPerSecondSpan.textContent = goldPerSecond.toFixed(1);
+
+  // Enable/disable upgrade button and update cost
+  if (gold >= upgradeCost) {
+    upgradeButton.disabled = false;
+    upgradeButton.textContent = `Upgrade (Cost: ${upgradeCost} gold)`;
+    upgradeButton.classList.add('clickable');
+  } else {
+    upgradeButton.disabled = true;
+    upgradeButton.textContent = `Upgrade (Cost: ${upgradeCost} gold)`;
+    upgradeButton.classList.remove('clickable');
+  }
 }
 
 // Upgrade gold per second and increase cost dynamically
 function upgrade() {
-	gold -= upgradeCost;
-	goldPerSecond *= 1.5;
-	goldPerSecond = Math.round(goldPerSecond * 100) / 100;
-	
-	upgradeCost *= 1.2;
-	upgradeCost = Math.round(upgradeCost);
-	
-	updateGoldCounter();
+  if (gold >= upgradeCost) {
+    gold -= upgradeCost;
+    goldPerSecond *= 1.5;
+    upgradeCost *= 1.2;
+    upgradeCost = Math.round(upgradeCost);
+    updateGoldCounter();
+  }
 }
 
 // Start game loop
