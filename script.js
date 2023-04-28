3bd2ebc48280c7ab20ac9044714342f0aab72f63
sivlin <sivlin@gmail.com>
2023-04-27 21:52:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,123 +1,118 @@
-class Game {
-  constructor() {
-    this.stats = new Stats();
-    this.buttons = new ButtonCollection();
-    this.updateInterval = null;
+let gold = 0;
+let goldPerSecond = 1;
+let goldDisplay = document.getElementById("gold-display");
+let gpsDisplay = document.getElementById("gps-display");
+
+class StatTracker {
+  constructor(id, initialValue) {
+    this.id = id;
+    this.value = initialValue;
+    this.element = document.getElementById(id);
+    this.updateDisplay();
   }
 
-  start() {
-    this.updateInterval = setInterval(() => {
-      this.stats.update();
-      this.updateStats();
-    }, 1000);
+  increment(value) {
+    this.value += value;
+    this.updateDisplay();
   }
 
-  stop() {
-    clearInterval(this.updateInterval);
-  }
-
-  updateStats() {
-    this.stats.updateDOM();
-    this.buttons.updateDOM();
-  }
-}
-
-class Stats {
-  constructor() {
-    this.gold = new Stat('Gold', 0, 1);
-    this.customStat = new Stat('Custom', 0, 0.1); // Example custom stat
-  }
-
-  update() {
-    this.gold.increment();
-    this.customStat.increment(); // Example custom stat
-  }
-
-  updateDOM() {
-    this.gold.updateDOM();
-    this.customStat.updateDOM(); // Example custom stat
-  }
-}
-
-class Stat {
-  constructor(name, value, increment) {
-    this.name = name;
-    this.value = value;
-    this.increment = increment;
-    this.element = null;
-  }
-
-  increment() {
-    this.value += this.increment;
-  }
-
-  updateDOM() {
-    if (!this.element) {
-      this.element = document.createElement('span');
-      this.element.textContent = `${this.name}: ${this.value.toFixed(1)}`;
-      document.body.appendChild(this.element);
-    } else {
-      this.element.textContent = `${this.name}: ${this.value.toFixed(1)}`;
-    }
-  }
-}
-
-class ButtonCollection {
-  constructor() {
-    this.buttons = [];
-  }
-
-  add(button) {
-    this.buttons.push(button);
-  }
-
-  updateDOM() {
-    this.buttons.forEach(button => button.updateDOM());
+  updateDisplay() {
+    this.element.textContent = `${this.id}: ${this.value}`;
   }
 }
 
 class Button {
-  constructor(name, cost, effect) {
-    this.name = name;
+  constructor(id, cost, gps) {
+    this.id = id;
     this.cost = cost;
-    this.effect = effect;
-    this.element = null;
+    this.gps = gps;
+    this.level = 0;
+    this.element = document.getElementById(id);
+    this.element.addEventListener("click", () => {
+      if (this.canAfford()) {
+        this.payCost();
+        this.levelUp();
+      }
+    });
+    this.updateButtonText();
   }
 
-  buy() {
-    if (game.stats.gold.value >= this.cost) {
-      game.stats.gold.value -= this.cost;
-      this.cost *= 2;
-      this.effect();
-      this.updateDOM();
-    }
+  canAfford() {
+    return gold >= this.cost;
   }
 
-  updateDOM() {
-    if (!this.element) {
-      this.element = document.createElement('button');
-      this.element.textContent = `${this.name} (Cost: ${this.cost})`;
-      document.body.appendChild(this.element);
-      this.element.addEventListener('click', () => this.buy());
-    } else {
-      this.element.textContent = `${this.name} (Cost: ${this.cost})`;
-    }
+  payCost() {
+    gold -= this.cost;
+    goldDisplay.textContent = `Gold: ${gold}`;
+  }
+
+  levelUp() {
+    this.level++;
+    goldPerSecond += this.gps;
+    this.cost = Math.floor(this.cost * 1.2);
+    this.updateButtonText();
+  }
+
+  updateButtonText() {
+    this.element.textContent = `${this.id} (Level ${this.level})\nCost: ${this.cost} Gold\n+${this.gps} GPS`;
   }
 }
 
-// Initialize game
-const game = new Game();
+class UpgradeButton {
+  constructor(id, baseCost, baseGPS, upgradeGPS) {
+    this.id = id;
+    this.cost = baseCost;
+    this.baseGPS = baseGPS;
+    this.upgradeGPS = upgradeGPS;
+    this.level = 0;
+    this.element = document.getElementById(id);
+    this.element.addEventListener("click", () => {
+      if (this.canAfford()) {
+        this.payCost();
+        this.upgrade();
+        this.updateButtonText();
+      }
+    });
+    this.element.addEventListener("mouseover", () => {
+      const currentGPS = this.baseGPS + this.level * this.upgradeGPS;
+      const nextGPS = this.baseGPS + (this.level + 1) * this.upgradeGPS;
+      const tooltip = `Current GPS: ${currentGPS}\nNext GPS: ${nextGPS}`;
+      this.element.setAttribute("title", tooltip);
+    });
+    this.updateButtonText();
+  }
 
-// Add stats
-game.stats.gold.element = document.getElementById('gold-counter'); // Gold is a special case
-game.stats.customStat.element = document.createElement('span'); // Example custom stat
-document.body.appendChild(game.stats.customStat.element); // Example custom stat
+  canAfford() {
+    return gold >= this.cost;
+  }
 
-// Add buttons
-const upgradeButton = new Button('Upgrade', 10, () => {
-  game.stats.gold.increment += 1;
-});
-game.buttons.add(upgradeButton);
+  payCost() {
+    gold -= this.cost;
+  }
 
-// Start game loop
-game.start();
+  upgrade() {
+    this.level++;
+    goldPerSecond += this.upgradeGPS;
+    this.cost = Math.ceil(this.cost * 1.2);
+  }
+
+  updateButtonText() {
+    const currentGPS = this.baseGPS + this.level * this.upgradeGPS;
+    const nextGPS = this.baseGPS + (this.level + 1) * this.upgradeGPS;
+    this.element.textContent = `Upgrade\n${this.cost} gold\nCurrent GPS: ${currentGPS}\nNext GPS: ${nextGPS}`;
+  }
+}
+
+// initialize stats
+let gpsTracker = new StatTracker("GPS", goldPerSecond);
+
+// initialize buttons
+let goldButton = new Button("gold-button", 10, 1);
+let upgradeButton = new UpgradeButton("upgrade-button", 100, 1, 1);
+
+// main game loop
+setInterval(() => {
+  gold += goldPerSecond;
+  goldDisplay.textContent = `Gold: ${gold}`;
+  gpsTracker.updateDisplay();
+}, 1000);
