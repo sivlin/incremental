b86418bd80c98ed1cfefda426446cc7e53aeeb4c
sivlin <sivlin@gmail.com>
2023-04-27 21:54:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,118 +1,82 @@
-let gold = 0;
-let goldPerSecond = 1;
-let goldDisplay = document.getElementById("gold-display");
-let gpsDisplay = document.getElementById("gps-display");
-
 class StatTracker {
-  constructor(id, initialValue) {
-    this.id = id;
-    this.value = initialValue;
-    this.element = document.getElementById(id);
-    this.updateDisplay();
+  constructor() {
+    this.stats = {};
   }
-
-  increment(value) {
-    this.value += value;
-    this.updateDisplay();
+  
+  getStat(statName) {
+    if (!this.stats.hasOwnProperty(statName)) {
+      this.stats[statName] = 0;
+    }
+    
+    return this.stats[statName];
   }
-
-  updateDisplay() {
-    this.element.textContent = `${this.id}: ${this.value}`;
+  
+  setStat(statName, value) {
+    this.stats[statName] = value;
+  }
+  
+  incrementStat(statName, value) {
+    if (!this.stats.hasOwnProperty(statName)) {
+      this.stats[statName] = 0;
+    }
+    
+    this.stats[statName] += value;
   }
 }
 
 class Button {
-  constructor(id, cost, gps) {
-    this.id = id;
+  constructor(cost, multiplier, elementId, statTracker, upgradeButtonId) {
     this.cost = cost;
-    this.gps = gps;
-    this.level = 0;
-    this.element = document.getElementById(id);
-    this.element.addEventListener("click", () => {
-      if (this.canAfford()) {
-        this.payCost();
-        this.levelUp();
-      }
-    });
+    this.multiplier = multiplier;
+    this.element = document.getElementById(elementId);
+    this.statTracker = statTracker;
+    this.upgradeButton = document.getElementById(upgradeButtonId);
+    
     this.updateButtonText();
+    this.addClickHandler();
   }
-
-  canAfford() {
-    return gold >= this.cost;
-  }
-
-  payCost() {
-    gold -= this.cost;
-    goldDisplay.textContent = `Gold: ${gold}`;
-  }
-
-  levelUp() {
-    this.level++;
-    goldPerSecond += this.gps;
-    this.cost = Math.floor(this.cost * 1.2);
-    this.updateButtonText();
-  }
-
-  updateButtonText() {
-    this.element.textContent = `${this.id} (Level ${this.level})\nCost: ${this.cost} Gold\n+${this.gps} GPS`;
-  }
-}
-
-class UpgradeButton {
-  constructor(id, baseCost, baseGPS, upgradeGPS) {
-    this.id = id;
-    this.cost = baseCost;
-    this.baseGPS = baseGPS;
-    this.upgradeGPS = upgradeGPS;
-    this.level = 0;
-    this.element = document.getElementById(id);
+  
+  addClickHandler() {
     this.element.addEventListener("click", () => {
-      if (this.canAfford()) {
-        this.payCost();
-        this.upgrade();
+      if (this.statTracker.getStat("gold") >= this.cost) {
+        this.statTracker.incrementStat("gold", -this.cost);
+        this.statTracker.incrementStat("goldPerSecond", this.multiplier);
+        this.cost *= 2;
         this.updateButtonText();
+        this.upgradeButton.innerHTML = `Upgrade (Cost: ${this.cost} Gold, Increase: ${this.multiplier} Gold/s -> ${this.multiplier * 2} Gold/s)`;
       }
     });
-    this.element.addEventListener("mouseover", () => {
-      const currentGPS = this.baseGPS + this.level * this.upgradeGPS;
-      const nextGPS = this.baseGPS + (this.level + 1) * this.upgradeGPS;
-      const tooltip = `Current GPS: ${currentGPS}\nNext GPS: ${nextGPS}`;
-      this.element.setAttribute("title", tooltip);
-    });
-    this.updateButtonText();
   }
-
-  canAfford() {
-    return gold >= this.cost;
-  }
-
-  payCost() {
-    gold -= this.cost;
-  }
-
-  upgrade() {
-    this.level++;
-    goldPerSecond += this.upgradeGPS;
-    this.cost = Math.ceil(this.cost * 1.2);
-  }
-
+  
   updateButtonText() {
-    const currentGPS = this.baseGPS + this.level * this.upgradeGPS;
-    const nextGPS = this.baseGPS + (this.level + 1) * this.upgradeGPS;
-    this.element.textContent = `Upgrade\n${this.cost} gold\nCurrent GPS: ${currentGPS}\nNext GPS: ${nextGPS}`;
+    this.element.innerHTML = `Generate Gold (Currently producing ${this.multiplier} Gold/s, Cost: ${this.cost} Gold)`;
   }
 }
 
-// initialize stats
-let gpsTracker = new StatTracker("GPS", goldPerSecond);
+let statTracker = new StatTracker();
+statTracker.setStat("gold", 0);
+statTracker.setStat("goldPerSecond", 1);
 
-// initialize buttons
-let goldButton = new Button("gold-button", 10, 1);
-let upgradeButton = new UpgradeButton("upgrade-button", 100, 1, 1);
+let goldPerSecondElement = document.getElementById("gold-per-second");
 
-// main game loop
 setInterval(() => {
-  gold += goldPerSecond;
-  goldDisplay.textContent = `Gold: ${gold}`;
-  gpsTracker.updateDisplay();
+  let goldPerSecond = statTracker.getStat("goldPerSecond");
+  statTracker.incrementStat("gold", goldPerSecond);
+  goldPerSecondElement.innerHTML = `Gold/s: ${goldPerSecond}`;
 }, 1000);
+
+let button = new Button(10, 1, "gold-button", statTracker, "upgrade-button");
+
+let upgradeButton = document.getElementById("upgrade-button");
+upgradeButton.addEventListener("click", () => {
+  let currentCost = button.cost;
+  let currentMultiplier = button.multiplier;
+  if (statTracker.getStat("gold") >= currentCost) {
+    statTracker.incrementStat("gold", -currentCost);
+    statTracker.incrementStat("goldPerSecond", currentMultiplier);
+    button.cost *= 2;
+    button.multiplier *= 2;
+    button.updateButtonText();
+    upgradeButton.innerHTML = `Upgrade (Cost: ${button.cost} Gold, Increase: ${button.multiplier/2} Gold/s -> ${button.multiplier} Gold/s)`;
+  }
+});
